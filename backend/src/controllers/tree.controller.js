5819cd41488a7a2921d202eecaeab69bd8c57269
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-10-11 05:52:51 +0530

    Add controllers

---
+++ backend/src/controllers/tree.controller.js
@@ -0,0 +1,42 @@
+const httpStatus = require('http-status');
+const fetch = require('node-fetch');
+const { pick } = require('lodash');
+const ApiError = require('../utils/ApiError');
+const catchAsync = require('../utils/catchAsync');
+const { treeService } = require('../services');
+const config = require('../config/config');
+
+const createTree = catchAsync(async (req, res) => {
+  const tree = await treeService.createTree(req.body);
+  res.status(httpStatus.CREATED).send(tree);
+});
+
+const getTrees = catchAsync(async (req, res) => {
+  const filter = pick(req.query, ['common_name']);
+  const options = pick(req.query, ['sortBy', 'limit', 'page']);
+  const result = await treeService.queryTrees(filter, options);
+  res.send(result);
+});
+
+const getTree = catchAsync(async (req, res) => {
+  const tree = await treeService.getTreeById(req.params.treeId);
+  if (!tree) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Tree not found');
+  }
+  res.send(tree);
+});
+
+const searchTrees = catchAsync(async (req, res) => {
+  const query = req.params.query;
+  const url = 'https://trefle.io/api/v1/plants/search?token=' + config.trefleToken + '&q=' + query;
+  const response = await fetch(url);
+  const json = await response.json();
+  res.send(json);
+});
+
+module.exports = {
+  createTree,
+  getTrees,
+  getTree,
+  searchTrees,
+};
