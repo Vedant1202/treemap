4a0d66fd12d221d6db3bb2affc45d961074933d0
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-10-11 06:42:09 +0530

    Add deployed api url

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-    apiUrl: 'http://localhost:4050/v1/',
+    apiUrl: 'https://www.treemap.tk:5000/v1/',
     mapboxToken: 'pk.eyJ1IjoidmV5eWRhbnQiLCJhIjoiY2tnM253MGlwMGJwejJ3cGpnY2Z4MndoayJ9._yAhXrsBN98XzbyNFj_kHA',
 };
 
