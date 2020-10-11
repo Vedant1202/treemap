1d7bcabf92aa45e1324e19cd4875f9a0cfb52087
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-10-11 05:54:41 +0530

    Add config

---
+++ frontend/src/config/config.js
@@ -0,0 +1,6 @@
+const config = {
+    apiUrl: 'http://localhost:4050/v1/',
+    mapboxToken: 'pk.eyJ1IjoidmV5eWRhbnQiLCJhIjoiY2tnM253MGlwMGJwejJ3cGpnY2Z4MndoayJ9._yAhXrsBN98XzbyNFj_kHA',
+};
+
+export default config;
