44d415f69b4726a28da00341ac0877e3bd3363dd
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-10-11 05:53:44 +0530

    Add validations

---
+++ backend/src/validations/tree.validation.js
@@ -0,0 +1,41 @@
+const Joi = require('@hapi/joi');
+const { objectId } = require('./custom.validation');
+
+const createTree = {
+  body: Joi.object().keys({
+    common_name: Joi.string().required(),
+    scientific_name: Joi.string().required(),
+    image_url: Joi.string().required(),
+    latitude: Joi.string().required(),
+    longitude: Joi.string().required(),
+    user_by: Joi.object(),
+  }),
+};
+
+const getTrees = {
+  query: Joi.object().keys({
+    common_name: Joi.string(),
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+  }),
+};
+
+const getTree = {
+  params: Joi.object().keys({
+    treeId: Joi.string().custom(objectId),
+  }),
+};
+
+const searchTrees = {
+  params: Joi.object().keys({
+    query: Joi.string(),
+  }),
+};
+
+module.exports = {
+  createTree,
+  getTrees,
+  getTree,
+  searchTrees,
+};
