09d1e5b2f723eab002d1f1565f1d9aeb020119a2
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2021-04-30 01:58:20 +0530

    Updates

--- precache-manifest.5f526396068f42493fc33873cffa5eee.js
+++ precache-manifest.5f526396068f42493fc33873cffa5eee.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bf2fd640a23b8e497835bbdb1e8ba822",
+    "revision": "2f33be9484d4fc724201598940146252",
     "url": "/treemap/index.html"
   },
   {
-    "revision": "af0dcb920f35c5b6a017",
+    "revision": "b3cba9cbaba82c2b64e5",
     "url": "/treemap/static/css/2.220ccb42.chunk.css"
   },
   {
-    "revision": "86128de264c54c86cb11",
+    "revision": "35d3785f71ceb9d923ae",
     "url": "/treemap/static/css/main.ee5bdd3c.chunk.css"
   },
   {
-    "revision": "af0dcb920f35c5b6a017",
-    "url": "/treemap/static/js/2.a15269a8.chunk.js"
+    "revision": "b3cba9cbaba82c2b64e5",
+    "url": "/treemap/static/js/2.2e606a9d.chunk.js"
   },
   {
     "revision": "4c3647ec769f81f0a9eaae3ac99b42a9",
-    "url": "/treemap/static/js/2.a15269a8.chunk.js.LICENSE.txt"
+    "url": "/treemap/static/js/2.2e606a9d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "86128de264c54c86cb11",
-    "url": "/treemap/static/js/main.305b3596.chunk.js"
+    "revision": "35d3785f71ceb9d923ae",
+    "url": "/treemap/static/js/main.bd9af3df.chunk.js"
   },
   {
     "revision": "702a232a9367d09f920e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/treemap/precache-manifest.4f35361481b07e1949e681985bdc5d94.js"
+  "/treemap/precache-manifest.5f526396068f42493fc33873cffa5eee.js"
 );
 
 self.addEventListener('message', (event) => {
