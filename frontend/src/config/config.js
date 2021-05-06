8c2e0c30a89d246b62f10c3299399875c17ae4da
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2021-05-06 17:28:59 +0530

    update api key

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,7 +1,7 @@
 const config = {
     apiUrl: 'https://www.eudocia.ml:4055/v1/',
     mapboxToken: 'pk.eyJ1IjoidmV5eWRhbnQiLCJhIjoiY2tnM253MGlwMGJwejJ3cGpnY2Z4MndoayJ9._yAhXrsBN98XzbyNFj_kHA',
-    apiKey: 'oejhZ0SbHagay3f0ug2rwjjFJrZoSPBhOHMmfKMrv0k',
+    apiKey: 'Go9vwArLt_5ImyBBfRrWb1pN7w8m-XrtjLybeb2MZ6w',
 };
 
 export default config;
